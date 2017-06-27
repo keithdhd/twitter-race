324afd9d6e40fcf715d2727261fe20e80c7694eb
siansrd <sianrobinsondavies@gmail.com>
2017-06-27 17:57:25 +0100

    Remove google-react-maps

--- client/containers/RaceContainer.jsx
+++ client/containers/RaceContainer.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import io from 'socket.io-client'
 import RacerCharts from '../components/RacerCharts'
 import RacerForm from '../components/RacerForm'
-import RacerMap from '../components/RacerMap'
-import TweetMarker from '../components/TweetMarker'
-import {markerStyles} from '../components/markerStyles'
 
 class RaceContainer extends React.Component {
 
@@ -45,24 +42,14 @@ class RaceContainer extends React.Component {
   }
 
   render() {
-    if (this.state.racerCounts['may']) {
     return (
       <div>
         <RacerForm startRace={this.startRace.bind(this)}/>
         <RacerCharts racerCounts={this.state.racerCounts}/>
-        <RacerMap tweets={this.state.racerCounts['may']}/>
       </div>
-      )
-    } else {
-      return (
-      <div>
-        <RacerForm startRace={this.startRace.bind(this)}/>
-        <RacerCharts racerCounts={this.state.racerCounts}/>
-        <RacerMap />
-      </div>
-      )
-    }
+    )
   }
 }
+
 
 export default RaceContainer
