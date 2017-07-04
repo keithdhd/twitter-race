6102dfb9b5b23f3c9fde1321935db1838513b771
siansrd <sianrobinsondavies@gmail.com>
2017-07-04 15:59:40 +0100

    Configure webpack to have access to bulma css and sass

--- client/app.js
+++ client/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RaceContainer from './containers/RaceContainer';
+require('./main.css')
+require('bulma')
 
 window.onload = () => {
   ReactDOM.render(
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,30 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 var config = {
   entry: __dirname + '/app.js',
   output: {
-    path: __dirname + '/../public/js',
+    path: __dirname + '/../public',
     filename: 'bundle.js'
   },
   devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.css', '.sass']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      template: `${__dirname}/index.html`,
+      filename: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      // filename: 'style.css'
+      filename: (getPath) => {
+        console.log(getPath('[name].css'))
+        return getPath('[name].css')
+      }
+    }),
+  ],
   module: {
     rules: [
       {
@@ -17,7 +34,33 @@ var config = {
         options: {
           presets: ['react']
         }
-      }
+      },
+      // THIS WORKS
+      // {
+      //   test: /\.sass$/,
+      //   loader: [
+      //     'style-loader',
+      //     'css-loader',
+      //     'sass-loader',
+      //   ]
+      // },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        }),
+      },
+      {
+        test: /(\.sass$|\.scss$)/,
+        loader: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            'sass-loader',
+          ],
+          fallback: 'style-loader'
+        }),
+      },
     ]
   }
 };
